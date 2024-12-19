cdc9086ff96d7ebc006243c093526cc81b443a7e
realwater <pie1104@nate.com>
2024-12-19 11:38:20 +0900

    컨트랙트 추가

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
-# TikTrix Contract
+# Miracle-Game Contract
 
 ## Overview
-TikTrix is a Web3-based HTML5 game participation platform.
+This is a contract for storing MiraclePlay game score results.
 
 ## Key Features
-
-### Game Management
-- Creators can create and manage new game challenges.
-- Set game rules and challenge fees.
 
 ### Gameplay and Score Processing
 - Store users' game participation records.
 - Calculate scores and manage ranking systems.
 - Validate and record game results.
-
-### Game Challenge Participation
-- Process applications from fans for game participation.
-- Handle entry fee payments.
-
-### Game Challenge Rewards
-- Select winners and distribute rewards.
-- Allocate earnings to creators.
---
+++ contracts/score/MiracleGameEvent.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/access/AccessControl.sol";
+
+contract MiracleGameEvent is AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    event RegisterScore(
+        uint256 indexed yyyymmdd,
+        string indexed gameUid,
+        string scoreData,
+        uint256 timestamp
+    );
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(ADMIN_ROLE, msg.sender);
+    }
+
+    function grantAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(ADMIN_ROLE, account);
+    }
+
+    function revokeAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(ADMIN_ROLE, account);
+    }
+
+    function registerScore(
+        uint256 yyyymmdd,
+        string memory _gameUid,
+        string memory _scoreData
+    ) external onlyRole(ADMIN_ROLE) {
+        require(yyyymmdd >= 19000101 && yyyymmdd <= 99991231, "Invalid date format");
+        require(bytes(_gameUid).length > 0, "GameUid cannot be empty");
+        require(bytes(_scoreData).length > 0, "ScoreData cannot be empty");
+        
+        emit RegisterScore(yyyymmdd, _gameUid, _scoreData, block.timestamp);
+    }
+}
